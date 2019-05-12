2fca4912723fd399cfd282f71eb4ee03ba4c3db8
sirrotich <rotichtitus12@gmail.com>
2019-05-12 03:33:15 +0300

    view function of searched images

--- photos/views.py
+++ photos/views.py
@@ -11,3 +11,16 @@ def gallery(request):
 
        images=Image.objects.all()
        return render(request, 'gallery.html',{"images": images })
+
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_category(search_term)
+
+        message = f"{search_term}"
+        return render(request, 'search_results.html', {"message":message, "images": searched_images})
+
+    else:
+        message = "You haven't made any terms"
+        return render(request, 'search_results.html', {"message":message})
