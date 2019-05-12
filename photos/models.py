6c9c5a9e5d44014179f5d68f409849c668e64b7b
sirrotich <rotichtitus12@gmail.com>
2019-05-12 03:36:23 +0300

    define image class to take in parameters of our post

--- photos/models.py
+++ photos/models.py
@@ -18,3 +18,19 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Image(models.Model):
+    image = models.ImageField(upload_to ='gallery/')
+    image_name = models.CharField(max_length =20)
+    image_description = models.TextField(max_length =400)
+    image_location = models.ForeignKey(Location)
+    image_category = models.ForeignKey(Category)
+
+    def __str__(self):
+        return self.image_name
+
+    def save_image(self):
+        self.save()
+
+    class Meta:
+        ordering = ['image_name']
