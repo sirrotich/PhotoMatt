832935ac4381b064be8a5d93222bddb32edde88b
sirrotich <rotichtitus12@gmail.com>
2019-05-12 03:29:25 +0300

    create first model

--- photos/models.py
+++ photos/models.py
@@ -1,1 +1,14 @@
+from django.db import models
 
+# Create your models here.
+class Location(models.Model):
+    first_name = models.CharField(max_length =25)
+
+    def __str__(self):
+        return self.first_name
+
+    def save_location(self):
+        self.save()
+
+    class Meta:
+        ordering = ['first_name']
